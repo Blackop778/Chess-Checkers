5cf3b95187f53ee66fb60173d3c31faa39f52e2d
Blackop778 <nefalt@att.net>
2016-06-22 22:51:38 -0500

    Pawns are mostly done
    Just need to work on promotions

---
+++ src/main/java/blackop778/chess_checkers/chess/PawnAdvancement.java
@@ -0,0 +1,9 @@
+package blackop778.chess_checkers.chess;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class PawnAdvancement extends JPanel
+{
+
+}
--- src/main/java/blackop778/chess_checkers/pieces/Pawn.java
+++ src/main/java/blackop778/chess_checkers/pieces/Pawn.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
 import blackop778.chess_checkers.Chess_Checkers;
+import blackop778.chess_checkers.Utilities;
 
 public class Pawn extends ChessPiece
 {
@@ -70,22 +71,117 @@ public class Pawn extends ChessPiece
 				Chess_Checkers.board[x][y] = this;
 			}
 		}
+		if(x - 1 > -1)
+		{
+			if(Chess_Checkers.board[x - 1][y + yOffset] instanceof ChessPiece
+					&& Chess_Checkers.board[x - 1][y + yOffset].black != black)
+			{
+				if((black && !blackKingInCheck) || (!black && !whiteKingInCheck))
+				{
+					validLocations.add(new Point(x - 1, y + yOffset));
+				}
+				else
+				{
+					Piece replacingPiece = Chess_Checkers.board[x - 1][y + yOffset];
+					Chess_Checkers.board[x - 1][y + yOffset] = this;
+					Chess_Checkers.board[x][y] = new Empty();
+					if(!isKingInCheck(black))
+					{
+						validLocations.add(new Point(x - 1, y + yOffset));
+					}
+					Chess_Checkers.board[x - 1][y + yOffset] = replacingPiece;
+					Chess_Checkers.board[x][y] = this;
+				}
+			}
+		}
+		if(x + 1 < 8)
+		{
+			if(Chess_Checkers.board[x + 1][y + yOffset] instanceof ChessPiece
+					&& Chess_Checkers.board[x + 1][y + yOffset].black != black)
+			{
+				if((black && !blackKingInCheck) || (!black && !whiteKingInCheck))
+				{
+					validLocations.add(new Point(x + 1, y + yOffset));
+				}
+				else
+				{
+					Piece replacingPiece = Chess_Checkers.board[x + 1][y + yOffset];
+					Chess_Checkers.board[x + 1][y + yOffset] = this;
+					Chess_Checkers.board[x][y] = new Empty();
+					if(!isKingInCheck(black))
+					{
+						validLocations.add(new Point(x + 1, y + yOffset));
+					}
+					Chess_Checkers.board[x + 1][y + yOffset] = replacingPiece;
+					Chess_Checkers.board[x][y] = this;
+				}
+			}
+		}
+		if(!moved)
+		{
+			if(Chess_Checkers.board[x][y + yOffset] instanceof Empty
+					&& Chess_Checkers.board[x][y + (yOffset * 2)] instanceof Empty)
+			{
+				if((black && !blackKingInCheck) || (!black && !whiteKingInCheck))
+				{
+					validLocations.add(new Point(x, y + (yOffset * 2)));
+				}
+				else
+				{
+					Piece replacingPiece = Chess_Checkers.board[x][y + (yOffset * 2)];
+					Chess_Checkers.board[x][y + (yOffset * 2)] = this;
+					Chess_Checkers.board[x][y] = new Empty();
+					if(!isKingInCheck(black))
+					{
+						validLocations.add(new Point(x, y + (yOffset * 2)));
+					}
+					Chess_Checkers.board[x][y + (yOffset * 2)] = replacingPiece;
+					Chess_Checkers.board[x][y] = this;
+				}
+			}
+		}
 
 		Point[] array = new Point[0];
+		validLocations.trimToSize();
 		return validLocations.toArray(array);
 	}
 
 	@Override
 	public void select(int x, int y)
 	{
-		// TODO Auto-generated method stub
-
+		if(black == Chess_Checkers.blackTurn)
+		{
+			Chess_Checkers.unselectAll();
+			Point[] locations = getValidLocations(x, y);
+			for(Point point : locations)
+			{
+				if(point != null)
+				{
+					selected = true;
+					Chess_Checkers.board[point.x][point.y].possible = true;
+					Chess_Checkers.board[point.x][point.y].selector = this;
+				}
+			}
+		}
 	}
 
 	@Override
 	public void move(int x, int y)
 	{
-		// TODO Auto-generated method stub
-
+		moved = true;
+		Chess_Checkers.unselectAll();
+		Chess_Checkers.blackTurn = Utilities.opposite(Chess_Checkers.blackTurn);
+		findSelfLoop: for(int i = 0; i < 8; i++)
+		{
+			for(int n = 0; n < 8; n++)
+			{
+				if(Chess_Checkers.board[i][n].equals(this))
+				{
+					Chess_Checkers.board[i][n] = new Empty();
+					break findSelfLoop;
+				}
+			}
+		}
+		Chess_Checkers.board[x][y] = this;
 	}
 }
