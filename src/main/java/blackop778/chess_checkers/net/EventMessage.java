d7113a9e87cc2ffa9ce079024a5139499d015d48
Blackop778 <nefalt@att.net>
2016-11-21 23:18:01 -0600

    Begin work on multiplayer over internet

---
+++ src/main/java/blackop778/chess_checkers/net/EventMessage.java
@@ -0,0 +1,11 @@
+package blackop778.chess_checkers.net;
+
+public class EventMessage {
+    private EventMessage(String coordinate1, String coordinate2) {
+
+    }
+
+    public static void instantiate(String coordinate1, String coordinate2) {
+
+    }
+}
---
+++ src/main/java/blackop778/chess_checkers/net/Server.java
@@ -0,0 +1,11 @@
+package blackop778.chess_checkers.net;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class Server extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+
+    }
+}
