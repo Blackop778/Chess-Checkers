a3af162a0ad6ce60a60b8e3673dfceb4e8acf541
Blackop778 <nefalt@att.net>
2016-11-21 22:48:57 -0600

    Fixed kings not detecting each other
    Also fixed the formatter

--- src/main/java/blackop778/chess_checkers/pieces/ChessPiece.java
+++ src/main/java/blackop778/chess_checkers/pieces/ChessPiece.java
@@ -7,265 +7,235 @@ import javax.swing.JOptionPane;
 import blackop778.chess_checkers.Chess_Checkers;
 import blackop778.chess_checkers.Utilities;
 
-public abstract class ChessPiece extends Piece
-{
-	public abstract Point[] getValidLocations(int x, int y);
+public abstract class ChessPiece extends Piece {
+    public abstract Point[] getValidLocations(int x, int y);
 
-	public static Point doubleMovePawn;
+    public static Point doubleMovePawn;
 
-	public static int pawnCaptureCount;
+    public static int pawnCaptureCount;
 
-	public static boolean threefoldRepetition;
+    public static boolean threefoldRepetition;
 
-	@Override
-	public void select(int x, int y)
-	{
-		if(black == Chess_Checkers.blackTurn)
-		{
-			Chess_Checkers.unselectAll();
-			Point[] locations = getValidLocations(x, y);
-			for(Point point : locations)
-			{
-				if(point != null)
-				{
-					selected = true;
-					Chess_Checkers.board[point.x][point.y].possible = true;
-					Chess_Checkers.board[point.x][point.y].selector = this;
-				}
-			}
+    @Override
+    public void select(int x, int y) {
+	if (black == Chess_Checkers.blackTurn) {
+	    Chess_Checkers.unselectAll();
+	    Point[] locations = getValidLocations(x, y);
+	    for (Point point : locations) {
+		if (point != null) {
+		    selected = true;
+		    Chess_Checkers.board[point.x][point.y].possible = true;
+		    Chess_Checkers.board[point.x][point.y].selector = this;
 		}
+	    }
+	}
+    }
+
+    @Override
+    // Remember King and Pawn have their own implementations so edit them too
+    public void move(int x, int y) {
+	Chess_Checkers.unselectAll();
+	Chess_Checkers.blackTurn = !Chess_Checkers.blackTurn;
+	if (doubleMovePawn != null) {
+	    Pawn pawn = (Pawn) Chess_Checkers.board[doubleMovePawn.x][doubleMovePawn.y];
+	    pawn.lastMoveDouble = false;
+	    doubleMovePawn = null;
+	}
+	findSelfLoop: for (int i = 0; i < 8; i++) {
+	    for (int n = 0; n < 8; n++) {
+		if (Chess_Checkers.board[i][n].equals(this)) {
+		    Chess_Checkers.board[i][n] = new Empty();
+		    break findSelfLoop;
+		}
+	    }
+	}
+	if (Chess_Checkers.board[x][y] instanceof Empty)
+	    pawnCaptureCount++;
+	else
+	    pawnCaptureCount = 0;
+	Chess_Checkers.board[x][y] = this;
+	endGameCheck();
+    }
+
+    public static boolean canMove(boolean black) {
+	for (int x = 0; x < 8; x++) {
+	    for (int y = 0; y < 8; y++) {
+		if (Chess_Checkers.board[x][y].black == black && Chess_Checkers.board[x][y] instanceof ChessPiece) {
+		    ChessPiece piece = (ChessPiece) Chess_Checkers.board[x][y];
+		    if (!Utilities.isArrayEmpty(piece.getValidLocations(x, y)))
+			return true;
+		}
+	    }
 	}
 
-	@Override
-	// Remember King and Pawn have their own implementations so edit them too
-	public void move(int x, int y)
-	{
-		Chess_Checkers.unselectAll();
-		Chess_Checkers.blackTurn = !Chess_Checkers.blackTurn;
-		if(doubleMovePawn != null)
-		{
-			Pawn pawn = (Pawn) Chess_Checkers.board[doubleMovePawn.x][doubleMovePawn.y];
-			pawn.lastMoveDouble = false;
-			doubleMovePawn = null;
-		}
-		findSelfLoop: for(int i = 0; i < 8; i++)
-		{
-			for(int n = 0; n < 8; n++)
-			{
-				if(Chess_Checkers.board[i][n].equals(this))
-				{
-					Chess_Checkers.board[i][n] = new Empty();
-					break findSelfLoop;
-				}
-			}
-		}
-		if(Chess_Checkers.board[x][y] instanceof Empty)
-			pawnCaptureCount++;
-		else
-			pawnCaptureCount = 0;
-		Chess_Checkers.board[x][y] = this;
-		endGameCheck();
-	}
+	return false;
+    }
 
-	public static boolean canMove(boolean black)
-	{
-		for(int x = 0; x < 8; x++)
-		{
-			for(int y = 0; y < 8; y++)
-			{
-				if(Chess_Checkers.board[x][y].black == black && Chess_Checkers.board[x][y] instanceof ChessPiece)
-				{
-					ChessPiece piece = (ChessPiece) Chess_Checkers.board[x][y];
-					if(!Utilities.isArrayEmpty(piece.getValidLocations(x, y)))
+    public static final boolean isKingInCheck(boolean black) {
+	for (int x = 0; x < 8; x++) {
+	    for (int y = 0; y < 8; y++) {
+		if (Chess_Checkers.board[x][y] instanceof King && Chess_Checkers.board[x][y].black == black) {
+		    int currentX = x;
+		    int currentY = y;
+		    int changeX = 0;
+		    int changeY = -1;
+
+		    // Check rooks, bishops, kings, and queens
+		    for (int i = 0; i < 8; i++) {
+			boolean done = false;
+			boolean first = true;
+			while (!done) {
+			    currentX += changeX;
+			    currentY += changeY;
+			    if (currentX > -1 && currentX < 8) {
+				if (currentY > -1 && currentY < 8) {
+				    // Check kings
+				    if (first) {
+					first = false;
+					if (Chess_Checkers.board[currentX][currentY] instanceof King) {
+					    if (Chess_Checkers.board[currentX][currentY].black != black)
 						return true;
+					}
+				    }
+				    // If we found a piece
+				    if (Chess_Checkers.board[currentX][currentY] instanceof ChessPiece) {
+					done = true;
+					// If the piece we found is a different
+					// color
+					if (Chess_Checkers.board[currentX][currentY].black != black) {
+					    // No sense in doing extra checks
+					    // (Oh no! One extra OR statement!
+					    // The horror!)
+					    if (i < 4) {
+						// If it can murder us
+						if (Chess_Checkers.board[currentX][currentY] instanceof Rook
+							|| Chess_Checkers.board[currentX][currentY] instanceof Queen) {
+						    return true;
+						}
+					    } else {
+						if (Chess_Checkers.board[currentX][currentY] instanceof Bishop
+							|| Chess_Checkers.board[currentX][currentY] instanceof Queen) {
+						    return true;
+						}
+					    }
+					}
+				    }
+				} else {
+				    done = true;
 				}
+			    } else {
+				done = true;
+			    }
 			}
-		}
+			currentX = x;
+			currentY = y;
+			// Check all the directions bishops, rooks, and queens
+			// can come from
+			switch (i) {
+			case 0:
+			    changeY = 0;
+			    changeX = 1;
+			    break;
+			case 1:
+			    changeY = 1;
+			    changeX = 0;
+			    break;
+			case 2:
+			    changeY = 0;
+			    changeX = -1;
+			    break;
+			case 3:
+			    changeX = 1;
+			    changeY = -1;
+			    break;
+			case 4:
+			    changeY = 1;
+			    changeX = 1;
+			    break;
+			case 5:
+			    changeY = 1;
+			    changeX = -1;
+			    break;
+			case 6:
+			    changeY = -1;
+			    changeX = -1;
+			    break;
+			}
+		    }
+		    // Check for pawns
+		    if (black) {
+			if (y + 1 < 8) {
+			    if (x - 1 > -1) {
+				if (Chess_Checkers.board[x - 1][y + 1].black != black
+					&& Chess_Checkers.board[x - 1][y + 1] instanceof Pawn)
+				    return true;
+			    }
+			    if (x + 1 < 8) {
+				if (Chess_Checkers.board[x + 1][y + 1].black != black
+					&& Chess_Checkers.board[x + 1][y + 1] instanceof Pawn)
+				    return true;
+			    }
+			}
+		    } else {
+			if (y - 1 > -1) {
+			    if (x - 1 > -1) {
+				if (Chess_Checkers.board[x - 1][y - 1].black != black
+					&& Chess_Checkers.board[x - 1][y - 1] instanceof Pawn)
+				    return true;
+			    }
+			    if (x + 1 < 8) {
+				if (Chess_Checkers.board[x + 1][y - 1].black != black
+					&& Chess_Checkers.board[x + 1][y - 1] instanceof Pawn)
+				    return true;
+			    }
+			}
+		    }
+		    // Check Knights
+		    Point[] xYChanges = { new Point(-2, -1), new Point(-2, 1), new Point(-1, -2), new Point(-1, 2),
+			    new Point(1, -2), new Point(1, 2), new Point(2, -1), new Point(2, 1) };
 
-		return false;
-	}
-
-	public static final boolean isKingInCheck(boolean black)
-	{
-		for(int x = 0; x < 8; x++)
-		{
-			for(int y = 0; y < 8; y++)
-			{
-				if(Chess_Checkers.board[x][y] instanceof King && Chess_Checkers.board[x][y].black == black)
-				{
-					int currentX = x;
-					int currentY = y;
-					int changeX = 0;
-					int changeY = -1;
-
-					for(int i = 0; i < 8; i++)
-					{
-						boolean done = false;
-						while(!done)
-						{
-							currentX += changeX;
-							currentY += changeY;
-							if(currentX > -1 && currentX < 8)
-							{
-								if(currentY > -1 && currentY < 8)
-								{
-									if(Chess_Checkers.board[currentX][currentY] instanceof ChessPiece)
-									{
-										done = true;
-										if(Chess_Checkers.board[currentX][currentY].black != black)
-										{
-											if(i < 4)
-											{
-												if(Chess_Checkers.board[currentX][currentY] instanceof Rook
-														|| Chess_Checkers.board[currentX][currentY] instanceof Queen)
-												{
-													return true;
-												}
-											}
-											else
-											{
-												if(Chess_Checkers.board[currentX][currentY] instanceof Bishop
-														|| Chess_Checkers.board[currentX][currentY] instanceof Queen)
-												{
-													return true;
-												}
-											}
-										}
-									}
-								}
-								else
-								{
-									done = true;
-								}
-							}
-							else
-							{
-								done = true;
-							}
-						}
-						currentX = x;
-						currentY = y;
-						switch(i)
-						{
-							case 0:
-								changeY = 0;
-								changeX = 1;
-								break;
-							case 1:
-								changeY = 1;
-								changeX = 0;
-								break;
-							case 2:
-								changeY = 0;
-								changeX = -1;
-								break;
-							case 3:
-								changeX = 1;
-								changeY = -1;
-								break;
-							case 4:
-								changeY = 1;
-								changeX = 1;
-								break;
-							case 5:
-								changeY = 1;
-								changeX = -1;
-								break;
-							case 6:
-								changeY = -1;
-								changeX = -1;
-								break;
-						}
-					}
-					if(black)
-					{
-						if(y + 1 < 8)
-						{
-							if(x - 1 > -1)
-							{
-								if(Chess_Checkers.board[x - 1][y + 1].black != black
-										&& Chess_Checkers.board[x - 1][y + 1] instanceof Pawn)
-									return true;
-							}
-							if(x + 1 < 8)
-							{
-								if(Chess_Checkers.board[x + 1][y + 1].black != black
-										&& Chess_Checkers.board[x + 1][y + 1] instanceof Pawn)
-									return true;
-							}
-						}
-					}
-					else
-					{
-						if(y - 1 > -1)
-						{
-							if(x - 1 > -1)
-							{
-								if(Chess_Checkers.board[x - 1][y - 1].black != black
-										&& Chess_Checkers.board[x - 1][y - 1] instanceof Pawn)
-									return true;
-							}
-							if(x + 1 < 8)
-							{
-								if(Chess_Checkers.board[x + 1][y - 1].black != black
-										&& Chess_Checkers.board[x + 1][y - 1] instanceof Pawn)
-									return true;
-							}
-						}
-					}
-					Point[] xYChanges = {new Point(-2, -1), new Point(-2, 1), new Point(-1, -2), new Point(-1, 2),
-							new Point(1, -2), new Point(1, 2), new Point(2, -1), new Point(2, 1)};
-
-					for(Point point : xYChanges)
-					{
-						int xChange = point.x;
-						int yChange = point.y;
-						if(x + xChange > -1 && x + xChange < 8)
-						{
-							// Ensure the changes remain within the borders of
-							// the board
-							if(y + yChange > -1 && y + yChange < 8)
-							{
-								if(Chess_Checkers.board[x + xChange][y + yChange].black != black
-										&& Chess_Checkers.board[x + xChange][y + yChange] instanceof Knight)
-								{
-									return true;
-								}
-							}
-						}
-					}
+		    for (Point point : xYChanges) {
+			int xChange = point.x;
+			int yChange = point.y;
+			if (x + xChange > -1 && x + xChange < 8) {
+			    // Ensure the changes remain within the borders of
+			    // the board
+			    if (y + yChange > -1 && y + yChange < 8) {
+				if (Chess_Checkers.board[x + xChange][y + yChange].black != black
+					&& Chess_Checkers.board[x + xChange][y + yChange] instanceof Knight) {
+				    return true;
 				}
+			    }
 			}
+		    }
+		    return false;
 		}
-		return false;
+	    }
 	}
+	// This shouldn't be reached, because that means the given color has no
+	// king
+	return false;
+    }
 
-	public void endGameCheck()
-	{
-		if(pawnCaptureCount == 50)
-		{
-			Chess_Checkers.gameOver = true;
-			JOptionPane.showMessageDialog(null,
-					"50 turns have passed since a piece has been taken or a pawn has moved. The game is a draw.",
-					"Deadlock has been reached", JOptionPane.INFORMATION_MESSAGE);
-		}
-		else if(!canMove(!black))
-		{
-			Chess_Checkers.gameOver = true;
-			if(isKingInCheck(!black))
-			{
-				String winner = black ? "black" : "white";
-				JOptionPane.showMessageDialog(null,
-						"Congratulations, " + winner + " wins. Exit this message and click on the board to restart.",
-						"A Champion has been decided", JOptionPane.INFORMATION_MESSAGE);
-			}
-			else
-			{
-				String cause = !black ? "black" : "white";
-				JOptionPane.showMessageDialog(null,
-						"The game is a draw because " + cause + " cannot move but isn't in check.",
-						"Deadlock has been reached", JOptionPane.INFORMATION_MESSAGE);
-			}
-		}
+    public void endGameCheck() {
+	if (pawnCaptureCount == 50) {
+	    Chess_Checkers.gameOver = true;
+	    JOptionPane.showMessageDialog(null,
+		    "50 turns have passed since a piece has been taken or a pawn has moved. The game is a draw.",
+		    "Deadlock has been reached", JOptionPane.INFORMATION_MESSAGE);
+	} else if (!canMove(!black)) {
+	    Chess_Checkers.gameOver = true;
+	    if (isKingInCheck(!black)) {
+		String winner = black ? "black" : "white";
+		JOptionPane.showMessageDialog(null,
+			"Congratulations, " + winner + " wins. Exit this message and click on the board to restart.",
+			"A Champion has been decided", JOptionPane.INFORMATION_MESSAGE);
+	    } else {
+		String cause = !black ? "black" : "white";
+		JOptionPane.showMessageDialog(null,
+			"The game is a draw because " + cause + " cannot move but isn't in check.",
+			"Deadlock has been reached", JOptionPane.INFORMATION_MESSAGE);
+	    }
 	}
+    }
 }
--- src/main/java/blackop778/chess_checkers/pieces/King.java
+++ src/main/java/blackop778/chess_checkers/pieces/King.java
@@ -11,298 +11,243 @@ import javax.imageio.ImageIO;
 
 import blackop778.chess_checkers.Chess_Checkers;
 
-public class King extends ChessPiece
-{
-	private boolean moved;
+public class King extends ChessPiece {
+    private boolean moved;
 
-	public King(boolean black)
-	{
-		this.black = black;
-		this.selected = false;
-		this.selector = null;
-		this.possible = false;
-		this.moved = false;
+    public King(boolean black) {
+	this.black = black;
+	this.selected = false;
+	this.selector = null;
+	this.possible = false;
+	this.moved = false;
+    }
+
+    @Override
+    public void drawSelf(Graphics g, int x, int y) {
+	if (possible) {
+	    g.setColor(Color.YELLOW);
+	    g.fillRect(x + 2, y + 2, 86, 86);
 	}
-
-	@Override
-	public void drawSelf(Graphics g, int x, int y)
-	{
-		if(possible)
-		{
-			g.setColor(Color.YELLOW);
-			g.fillRect(x + 2, y + 2, 86, 86);
-		}
-		String color = black ? "Black" : "White";
-		File image = new File("resources\\" + color + "King.png");
-		try
-		{
-			g.drawImage(ImageIO.read(image), x, y, null);
-		}
-		catch(IOException e)
-		{
-			e.printStackTrace();
-		}
+	String color = black ? "Black" : "White";
+	File image = new File("resources\\" + color + "King.png");
+	try {
+	    g.drawImage(ImageIO.read(image), x, y, null);
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
+    }
 
-	@Override
-	public void move(int x, int y)
-	{
-		Chess_Checkers.unselectAll();
-		Chess_Checkers.blackTurn = !Chess_Checkers.blackTurn;
-		if(doubleMovePawn != null)
-		{
-			Pawn pawn = (Pawn) Chess_Checkers.board[doubleMovePawn.x][doubleMovePawn.y];
-			pawn.lastMoveDouble = false;
-			doubleMovePawn = null;
+    @Override
+    public void move(int x, int y) {
+	Chess_Checkers.unselectAll();
+	Chess_Checkers.blackTurn = !Chess_Checkers.blackTurn;
+	if (doubleMovePawn != null) {
+	    Pawn pawn = (Pawn) Chess_Checkers.board[doubleMovePawn.x][doubleMovePawn.y];
+	    pawn.lastMoveDouble = false;
+	    doubleMovePawn = null;
+	}
+	findSelfLoop: for (int i = 0; i < 8; i++) {
+	    for (int n = 0; n < 8; n++) {
+		if (Chess_Checkers.board[i][n].equals(this)) {
+		    Chess_Checkers.board[i][n] = new Empty();
+		    break findSelfLoop;
 		}
-		findSelfLoop: for(int i = 0; i < 8; i++)
-		{
-			for(int n = 0; n < 8; n++)
-			{
-				if(Chess_Checkers.board[i][n].equals(this))
-				{
-					Chess_Checkers.board[i][n] = new Empty();
-					break findSelfLoop;
+	    }
+	}
+	if (!moved) {
+	    if (x == 2) {
+		Chess_Checkers.board[3][y] = Chess_Checkers.board[0][y];
+		Chess_Checkers.board[0][y] = new Empty();
+	    } else if (x == 6) {
+		Chess_Checkers.board[5][y] = Chess_Checkers.board[7][y];
+		Chess_Checkers.board[7][y] = new Empty();
+	    }
+	}
+	if (Chess_Checkers.board[x][y] instanceof Empty)
+	    pawnCaptureCount++;
+	else
+	    pawnCaptureCount = 0;
+	Chess_Checkers.board[x][y] = this;
+	moved = true;
+	endGameCheck();
+    }
+
+    @Override
+    public Point[] getValidLocations(int x, int y) {
+	ArrayList<Point> validLocations = new ArrayList<>();
+
+	for (int xChange = -1; xChange < 2; xChange++) {
+	    for (int yChange = -1; yChange < 2; yChange++) {
+		if (xChange != 0 || yChange != 0) {
+		    if (x + xChange > -1 && x + xChange < 8) {
+			if (y + yChange > -1 && y + yChange < 8) {
+			    boolean check = false;
+			    if (Chess_Checkers.board[x + xChange][y + yChange] instanceof Empty) {
+				check = true;
+			    } else if (Chess_Checkers.board[x + xChange][y + yChange].black != black) {
+				check = true;
+			    }
+			    if (check) {
+				Piece replacingPiece = Chess_Checkers.board[x + xChange][y + yChange];
+				Chess_Checkers.board[x + xChange][y + yChange] = this;
+				Chess_Checkers.board[x][y] = new Empty();
+				if (!isKingInCheck(black)) {
+				    validLocations.add(new Point(x + xChange, y + yChange));
 				}
+				Chess_Checkers.board[x + xChange][y + yChange] = replacingPiece;
+				Chess_Checkers.board[x][y] = this;
+			    }
 			}
+		    }
 		}
-		if(!moved)
-		{
-			if(x == 2)
-			{
-				Chess_Checkers.board[3][y] = Chess_Checkers.board[0][y];
-				Chess_Checkers.board[0][y] = new Empty();
-			}
-			else if(x == 6)
-			{
-				Chess_Checkers.board[5][y] = Chess_Checkers.board[7][y];
-				Chess_Checkers.board[7][y] = new Empty();
-			}
-		}
-		if(Chess_Checkers.board[x][y] instanceof Empty)
-			pawnCaptureCount++;
-		else
-			pawnCaptureCount = 0;
-		Chess_Checkers.board[x][y] = this;
-		moved = true;
-		endGameCheck();
+	    }
 	}
 
-	@Override
-	public Point[] getValidLocations(int x, int y)
-	{
-		ArrayList<Point> validLocations = new ArrayList<Point>();
+	Integer sides = canCastle(x, y);
+	if (sides != null) {
+	    if (sides < 1) {
+		validLocations.add(new Point(2, y));
+	    }
+	    if (sides > -1) {
+		validLocations.add(new Point(6, y));
+	    }
+	}
 
-		for(int xChange = -1; xChange < 2; xChange++)
-		{
-			for(int yChange = -1; yChange < 2; yChange++)
-			{
-				if(xChange != 0 || yChange != 0)
-				{
-					if(x + xChange > -1 && x + xChange < 8)
-					{
-						if(y + yChange > -1 && y + yChange < 8)
-						{
-							boolean check = false;
-							if(Chess_Checkers.board[x + xChange][y + yChange] instanceof Empty)
-							{
-								check = true;
-							}
-							else if(Chess_Checkers.board[x + xChange][y + yChange].black != black)
-							{
-								check = true;
-							}
-							if(check)
-							{
-								Piece replacingPiece = Chess_Checkers.board[x + xChange][y + yChange];
-								Chess_Checkers.board[x + xChange][y + yChange] = this;
-								Chess_Checkers.board[x][y] = new Empty();
-								if(!isKingInCheck(black))
-								{
-									validLocations.add(new Point(x + xChange, y + yChange));
-								}
-								Chess_Checkers.board[x + xChange][y + yChange] = replacingPiece;
-								Chess_Checkers.board[x][y] = this;
-							}
-						}
-					}
+	Point[] array = new Point[0];
+	validLocations.trimToSize();
+	return validLocations.toArray(array);
+    }
+
+    /**
+     * 
+     * @param x
+     *            X of the king
+     * @param y
+     *            Y of the king
+     * @return Valid sides: Queen side is negative, King side is positive, both
+     *         is 0, neither is null
+     */
+    public Integer canCastle(int x, int y) {
+	// Queen side is negative, King side is positive, both is 0, neither is
+	// null
+	Integer sides = null;
+	if (!moved) {
+	    // Checks if the rooks have moved
+	    if (Chess_Checkers.board[0][y] instanceof Rook) {
+		Rook rook = (Rook) Chess_Checkers.board[0][y];
+		if (!rook.getMoved()) {
+		    sides = -1;
+		}
+	    }
+	    if (Chess_Checkers.board[7][y] instanceof Rook) {
+		Rook rook = (Rook) Chess_Checkers.board[7][y];
+		if (!rook.getMoved()) {
+		    if (sides != null)
+			sides = 0;
+		    else
+			sides = 1;
+		}
+	    }
+	    if (sides != null) {
+		// Checks if the space between the king and rook(s) is empty
+		if (sides < 1) {
+		    if (Chess_Checkers.board[3][y] instanceof Empty) {
+			if (Chess_Checkers.board[2][y] instanceof Empty) {
+			    if (Chess_Checkers.board[1][y] instanceof ChessPiece) {
+				if (sides != 0)
+				    sides = null;
+				else
+				    sides = 1;
+			    }
+			} else {
+			    if (sides != 0)
+				sides = null;
+			    else
+				sides = 1;
+			}
+		    } else {
+			if (sides != 0)
+			    sides = null;
+			else
+			    sides = 1;
+		    }
+		}
+		if (sides > -1) {
+		    if (Chess_Checkers.board[5][y] instanceof Empty) {
+			if (Chess_Checkers.board[6][y] instanceof ChessPiece) {
+			    if (sides != 0)
+				sides = null;
+			    else
+				sides = -1;
+			}
+		    } else {
+			if (sides != 0)
+			    sides = null;
+			else
+			    sides = -1;
+		    }
+		}
+		if (sides != null) {
+		    // King cannot move out of check
+		    if (!ChessPiece.isKingInCheck(black)) {
+			// King cannot pass through or move into check
+			if (sides < 1) {
+			    Piece piece = Chess_Checkers.board[3][y];
+			    Chess_Checkers.board[3][y] = this;
+			    Chess_Checkers.board[4][y] = new Empty();
+			    if (!ChessPiece.isKingInCheck(black)) {
+				Chess_Checkers.board[3][y] = piece;
+				piece = Chess_Checkers.board[2][y];
+				Chess_Checkers.board[2][y] = this;
+				if (ChessPiece.isKingInCheck(black)) {
+				    if (sides != 0)
+					sides = null;
+				    else
+					sides = 1;
 				}
+				Chess_Checkers.board[2][y] = piece;
+				Chess_Checkers.board[4][y] = this;
+			    } else {
+				Chess_Checkers.board[3][y] = piece;
+				Chess_Checkers.board[4][y] = this;
+				if (sides != 0)
+				    sides = null;
+				else
+				    sides = 1;
+			    }
 			}
-		}
-
-		Integer sides = canCastle(x, y);
-		if(sides != null)
-		{
-			if(sides < 1)
-			{
-				validLocations.add(new Point(2, y));
-			}
-			if(sides > -1)
-			{
-				validLocations.add(new Point(6, y));
-			}
-		}
-
-		Point[] array = new Point[0];
-		validLocations.trimToSize();
-		return validLocations.toArray(array);
-	}
-
-	public Integer canCastle(int x, int y)
-	{
-		// Queen side is negative, King side is positive, both is 0, neither
-		// is null
-		Integer sides = null;
-		if(!moved)
-		{
-			// Checks if the rooks have moved
-			if(Chess_Checkers.board[0][y] instanceof Rook)
-			{
-				Rook rook = (Rook) Chess_Checkers.board[0][y];
-				if(!rook.getMoved())
-				{
+			if (sides > -1) {
+			    Piece piece = Chess_Checkers.board[5][y];
+			    Chess_Checkers.board[5][y] = this;
+			    Chess_Checkers.board[4][y] = new Empty();
+			    if (!ChessPiece.isKingInCheck(black)) {
+				Chess_Checkers.board[5][y] = piece;
+				piece = Chess_Checkers.board[6][y];
+				Chess_Checkers.board[6][y] = this;
+				if (ChessPiece.isKingInCheck(black)) {
+				    if (sides != 0)
+					sides = null;
+				    else
 					sides = -1;
 				}
+				Chess_Checkers.board[6][y] = piece;
+				Chess_Checkers.board[4][y] = this;
+			    } else {
+				Chess_Checkers.board[5][y] = piece;
+				Chess_Checkers.board[4][y] = this;
+				if (sides != 0)
+				    sides = null;
+				else
+				    sides = -1;
+			    }
 			}
-			if(Chess_Checkers.board[7][y] instanceof Rook)
-			{
-				Rook rook = (Rook) Chess_Checkers.board[7][y];
-				if(!rook.getMoved())
-				{
-					if(sides != null)
-						sides = 0;
-					else
-						sides = 1;
-				}
-			}
-			if(sides != null)
-			{
-				// Checks if the space between the king and rook(s) is empty
-				if(sides < 1)
-				{
-					if(Chess_Checkers.board[3][y] instanceof Empty)
-					{
-						if(Chess_Checkers.board[2][y] instanceof Empty)
-						{
-							if(Chess_Checkers.board[1][y] instanceof ChessPiece)
-							{
-								if(sides != 0)
-									sides = null;
-								else
-									sides = 1;
-							}
-						}
-						else
-						{
-							if(sides != 0)
-								sides = null;
-							else
-								sides = 1;
-						}
-					}
-					else
-					{
-						if(sides != 0)
-							sides = null;
-						else
-							sides = 1;
-					}
-				}
-				if(sides > -1)
-				{
-					if(Chess_Checkers.board[5][y] instanceof Empty)
-					{
-						if(Chess_Checkers.board[6][y] instanceof ChessPiece)
-						{
-							if(sides != 0)
-								sides = null;
-							else
-								sides = -1;
-						}
-					}
-					else
-					{
-						if(sides != 0)
-							sides = null;
-						else
-							sides = -1;
-					}
-				}
-				if(sides != null)
-				{
-					// King cannot move out of check
-					if(!ChessPiece.isKingInCheck(black))
-					{
-						// King cannot pass through or move into check
-						if(sides < 1)
-						{
-							Piece piece = Chess_Checkers.board[3][y];
-							Chess_Checkers.board[3][y] = this;
-							Chess_Checkers.board[4][y] = new Empty();
-							if(!ChessPiece.isKingInCheck(black))
-							{
-								Chess_Checkers.board[3][y] = piece;
-								piece = Chess_Checkers.board[2][y];
-								Chess_Checkers.board[2][y] = this;
-								if(ChessPiece.isKingInCheck(black))
-								{
-									if(sides != 0)
-										sides = null;
-									else
-										sides = 1;
-								}
-								Chess_Checkers.board[2][y] = piece;
-								Chess_Checkers.board[4][y] = this;
-							}
-							else
-							{
-								Chess_Checkers.board[3][y] = piece;
-								Chess_Checkers.board[4][y] = this;
-								if(sides != 0)
-									sides = null;
-								else
-									sides = 1;
-							}
-						}
-						if(sides > -1)
-						{
-							Piece piece = Chess_Checkers.board[5][y];
-							Chess_Checkers.board[5][y] = this;
-							Chess_Checkers.board[4][y] = new Empty();
-							if(!ChessPiece.isKingInCheck(black))
-							{
-								Chess_Checkers.board[5][y] = piece;
-								piece = Chess_Checkers.board[6][y];
-								Chess_Checkers.board[6][y] = this;
-								if(ChessPiece.isKingInCheck(black))
-								{
-									if(sides != 0)
-										sides = null;
-									else
-										sides = -1;
-								}
-								Chess_Checkers.board[6][y] = piece;
-								Chess_Checkers.board[4][y] = this;
-							}
-							else
-							{
-								Chess_Checkers.board[5][y] = piece;
-								Chess_Checkers.board[4][y] = this;
-								if(sides != 0)
-									sides = null;
-								else
-									sides = -1;
-							}
-						}
-					}
-					else
-					{
-						sides = null;
-					}
+		    } else {
+			sides = null;
+		    }
 
-				}
-			}
 		}
-		return sides;
+	    }
 	}
+	return sides;
+    }
 }
