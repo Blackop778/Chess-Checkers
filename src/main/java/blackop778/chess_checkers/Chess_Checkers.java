1602af2cdf1e8ca7e021f47b4698454c031df615
Nathan <NSFaltermeier@bluevalleyk12.net>
2016-11-22 15:12:26 -0600

    Work on event messages

--- src/main/java/blackop778/chess_checkers/Chess_Checkers.java
+++ src/main/java/blackop778/chess_checkers/Chess_Checkers.java
@@ -18,158 +18,119 @@ import blackop778.chess_checkers.pieces.Piece;
 import blackop778.chess_checkers.pieces.Queen;
 import blackop778.chess_checkers.pieces.Rook;
 
-public abstract class Chess_Checkers
-{
-	public static Piece[][] board;
+public abstract class Chess_Checkers {
+    public static Piece[][] board;
 
-	public static boolean blackTurn;
-	public static boolean gameOver;
-	public static boolean gameIsCheckers;
-	public static boolean offerSurrender;
+    public static boolean blackTurn;
+    public static boolean gameOver;
+    public static boolean gameIsCheckers;
+    public static boolean offerSurrender;
 
-	public static void main(String[] args)
-	{
-		setupGame();
+    public static void main(String[] args) {
+	setupGame();
 
-		Chess_CheckersFrame frame = new Chess_CheckersFrame();
-		frame.setTitle("Chess-Checkers");
-		frame.setPreferredSize(new Dimension(727, 794));
-		frame.pack();
-		frame.setResizable(false);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
+	Chess_CheckersFrame frame = new Chess_CheckersFrame();
+	frame.setTitle("Chess-Checkers");
+	frame.setPreferredSize(new Dimension(727, 794));
+	frame.pack();
+	frame.setResizable(false);
+	frame.setLocationRelativeTo(null);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setVisible(true);
+    }
+
+    public static void setupGame() {
+	gameOver = false;
+	offerSurrender = false;
+	board = new Piece[8][8];
+	for (int i = 0; i < board.length; i++) {
+	    for (int n = 0; n < board[0].length; n++) {
+		board[i][n] = new Empty();
+	    }
 	}
 
-	public static void setupGame()
-	{
-		gameOver = false;
-		offerSurrender = false;
-		board = new Piece[8][8];
-		for(int i = 0; i < board.length; i++)
-		{
-			for(int n = 0; n < board[0].length; n++)
-			{
-				board[i][n] = new Empty();
-			}
-		}
-
-		String input = JOptionPane.showInputDialog(null, "Enter 'chess' to play chess or 'checkers' to play checkers.",
-				"Which game?", JOptionPane.QUESTION_MESSAGE);
-		if(input == null)
-			System.exit(0);
-		while(!input.equalsIgnoreCase("chess") && !input.equalsIgnoreCase("checkers"))
-		{
-			input = JOptionPane.showInputDialog(null, "Invalid input. Enter either 'chess' or 'checkers'.",
-					"Ohoes noes", JOptionPane.ERROR_MESSAGE);
-			if(input == null)
-				input = "null";
-		}
-
-		if(input.equalsIgnoreCase("checkers"))
-		{
-			ChessPiece.threefoldRepetition = false;
-			SnapshotStorage.initialize(false);
-			gameIsCheckers = true;
-			blackTurn = true;
-			for(int i = 0; i < board.length; i++)
-			{
-				for(int n = 0; n < board[0].length; n++)
-				{
-					if((i + n) % 2 == 1)
-					{
-						if(n < 3)
-						{
-							board[i][n] = new Checker(true, n + (i * 8));
-						}
-						else if(n > 4)
-						{
-							board[i][n] = new Checker(false, n + (i * 8));
-						}
-					}
-				}
-			}
-		}
-		else
-		{
-			ChessPiece.threefoldRepetition = true;
-			SnapshotStorage.initialize(true);
-			ChessPiece.doubleMovePawn = null;
-			ChessPiece.pawnCaptureCount = 0;
-			gameIsCheckers = false;
-			blackTurn = false;
-			for(int i = 0; i < board.length; i++)
-			{
-				for(int n = 0; n < board[0].length; n++)
-				{
-					switch(n)
-					{
-						case 0:
-							if(i == 0 || i == 7)
-							{
-								board[i][n] = new Rook(true);
-							}
-							else if(i == 1 || i == 6)
-							{
-								board[i][n] = new Knight(true);
-							}
-							else if(i == 2 || i == 5)
-							{
-								board[i][n] = new Bishop(true);
-							}
-							else if(i == 3)
-							{
-								board[i][n] = new Queen(true);
-							}
-							else if(i == 4)
-							{
-								board[i][n] = new King(true);
-							}
-							break;
-						case 1:
-							board[i][n] = new Pawn(true);
-							break;
-
-						case 6:
-							board[i][n] = new Pawn(false);
-							break;
-						case 7:
-							if(i == 0 || i == 7)
-							{
-								board[i][n] = new Rook(false);
-							}
-							else if(i == 1 || i == 6)
-							{
-								board[i][n] = new Knight(false);
-							}
-							else if(i == 2 || i == 5)
-							{
-								board[i][n] = new Bishop(false);
-							}
-							else if(i == 3)
-							{
-								board[i][n] = new Queen(false);
-							}
-							else if(i == 4)
-							{
-								board[i][n] = new King(false);
-							}
-							break;
-					}
-				}
-			}
-		}
+	String input = JOptionPane.showInputDialog(null, "Enter 'chess' to play chess or 'checkers' to play checkers.",
+		"Which game?", JOptionPane.QUESTION_MESSAGE);
+	if (input == null)
+	    System.exit(0);
+	while (!input.equalsIgnoreCase("chess") && !input.equalsIgnoreCase("checkers")) {
+	    input = JOptionPane.showInputDialog(null, "Invalid input. Enter either 'chess' or 'checkers'.",
+		    "Ohoes noes", JOptionPane.ERROR_MESSAGE);
+	    if (input == null)
+		input = "null";
 	}
 
-	public static void unselectAll()
-	{
-		for(Piece[] row : board)
-		{
-			for(Piece piece : row)
-			{
-				piece.possible = false;
-				piece.selected = false;
+	if (input.equalsIgnoreCase("checkers")) {
+	    ChessPiece.threefoldRepetition = false;
+	    SnapshotStorage.initialize(false);
+	    gameIsCheckers = true;
+	    blackTurn = true;
+	    for (int i = 0; i < board.length; i++) {
+		for (int n = 0; n < board[0].length; n++) {
+		    if ((i + n) % 2 == 1) {
+			if (n < 3) {
+			    board[i][n] = new Checker(true, n + (i * 8));
+			} else if (n > 4) {
+			    board[i][n] = new Checker(false, n + (i * 8));
 			}
+		    }
 		}
+	    }
+	} else {
+	    ChessPiece.threefoldRepetition = true;
+	    SnapshotStorage.initialize(true);
+	    ChessPiece.doubleMovePawn = null;
+	    ChessPiece.pawnCaptureCount = 0;
+	    gameIsCheckers = false;
+	    blackTurn = false;
+	    for (int i = 0; i < board.length; i++) {
+		for (int n = 0; n < board[0].length; n++) {
+		    switch (n) {
+		    case 0:
+			if (i == 0 || i == 7) {
+			    board[i][n] = new Rook(true);
+			} else if (i == 1 || i == 6) {
+			    board[i][n] = new Knight(true);
+			} else if (i == 2 || i == 5) {
+			    board[i][n] = new Bishop(true);
+			} else if (i == 3) {
+			    board[i][n] = new Queen(true);
+			} else if (i == 4) {
+			    board[i][n] = new King(true);
+			}
+			break;
+		    case 1:
+			board[i][n] = new Pawn(true);
+			break;
+
+		    case 6:
+			board[i][n] = new Pawn(false);
+			break;
+		    case 7:
+			if (i == 0 || i == 7) {
+			    board[i][n] = new Rook(false);
+			} else if (i == 1 || i == 6) {
+			    board[i][n] = new Knight(false);
+			} else if (i == 2 || i == 5) {
+			    board[i][n] = new Bishop(false);
+			} else if (i == 3) {
+			    board[i][n] = new Queen(false);
+			} else if (i == 4) {
+			    board[i][n] = new King(false);
+			}
+			break;
+		    }
+		}
+	    }
 	}
+    }
+
+    public static void unselectAll() {
+	for (Piece[] row : board) {
+	    for (Piece piece : row) {
+		piece.possible = false;
+		piece.selected = false;
+	    }
+	}
+    }
 }
--- src/main/java/blackop778/chess_checkers/net/EventMessage.java
+++ src/main/java/blackop778/chess_checkers/net/EventMessage.java
@@ -1,11 +1,30 @@
 package blackop778.chess_checkers.net;
 
 public class EventMessage {
-    private EventMessage(String coordinate1, String coordinate2) {
+    public final String coordinate1;
+    public final String coordinate2;
+    public final boolean offerSurrender;
 
+    private EventMessage(String coordinate1, String coordinate2, boolean offerSurrender) {
+	this.coordinate1 = coordinate1;
+	this.coordinate2 = coordinate2;
+	this.offerSurrender = offerSurrender;
     }
 
-    public static void instantiate(String coordinate1, String coordinate2) {
+    private EventMessage(String coordinate1, String coordinate2) {
+	this(coordinate1, coordinate2, false);
+    }
 
+    /**
+     * 
+     * @param coordinate1
+     * @param coordinate2
+     * @return Will be null if invalid input
+     */
+    public static EventMessage instantiate(String coordinate1, String coordinate2) {
+	if (coordinate1.matches("[A-G][1-8]") && coordinate2.matches("[A-G][1-8]")) {
+	    return new EventMessage(coordinate1, coordinate2);
+	}
+	return null;
     }
 }
