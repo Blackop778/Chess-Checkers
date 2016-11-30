4d38a04790c0d2b5e139a1eba6623e30b2afecbd
Nathan <NSFaltermeier@bluevalleyk12.net>
2016-11-30 07:40:51 -0600

    Added more methods to EventMessage

--- src/main/java/blackop778/chess_checkers/net/Client.java
+++ src/main/java/blackop778/chess_checkers/net/Client.java
@@ -1,13 +1,28 @@
 package blackop778.chess_checkers.net;
 
+import blackop778.chess_checkers.pieces.Piece;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
 public class Client extends ChannelInboundHandlerAdapter {
+
+    private Piece[][] board;
+    public final boolean black;
+    public final boolean gameIsCheckers;
+
+    public Client(boolean black, boolean gameIsCheckers) {
+	this.black = black;
+	this.gameIsCheckers = gameIsCheckers;
+    }
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
 	if (msg instanceof EventMessage) {
 	    EventMessage event = (EventMessage) msg;
 	}
+    }
+
+    public Piece[][] getBoard() {
+	return board;
     }
 }
--- src/main/java/blackop778/chess_checkers/net/EventMessage.java
+++ src/main/java/blackop778/chess_checkers/net/EventMessage.java
@@ -5,6 +5,19 @@ public class EventMessage {
     public final String coordinate2;
     public final boolean offerSurrender;
 
+    /**
+     * 
+     * @param coordinate1
+     * @param coordinate2
+     * @return Will be null if invalid input
+     */
+    public static EventMessage instantiate(String coordinate1, String coordinate2) {
+	if (coordinate1.matches("[A-H][1-8]") && coordinate2.matches("[A-H][1-8]")) {
+	    return new EventMessage(coordinate1, coordinate2);
+	}
+	return null;
+    }
+
     private EventMessage(String coordinate1, String coordinate2, boolean offerSurrender) {
 	this.coordinate1 = coordinate1;
 	this.coordinate2 = coordinate2;
@@ -44,17 +57,37 @@ public class EventMessage {
 	throw new InvalidCoordinateException();
     }
 
-    /**
-     * 
-     * @param coordinate1
-     * @param coordinate2
-     * @return Will be null if invalid input
-     */
-    public static EventMessage instantiate(String coordinate1, String coordinate2) {
-	if (coordinate1.matches("[A-H][1-8]") && coordinate2.matches("[A-H][1-8]")) {
-	    return new EventMessage(coordinate1, coordinate2);
+    public int getCoordinate2X() {
+	switch (coordinate2.charAt(0)) {
+	case 'A':
+	    return 0;
+	case 'B':
+	    return 1;
+	case 'C':
+	    return 2;
+	case 'D':
+	    return 3;
+	case 'E':
+	    return 4;
+	case 'F':
+	    return 5;
+	case 'G':
+	    return 6;
+	case 'H':
+	    return 7;
 	}
-	return null;
+	throw new InvalidCoordinateException();
+    }
+
+    public int getCoordinate2Y() {
+	int y = Integer.valueOf(new StringBuilder().append(coordinate2.charAt(1)).toString());
+	if (y > 0 && y < 9)
+	    return y - 1;
+	throw new InvalidCoordinateException();
+    }
+
+    public boolean getOfferSurrender() {
+	return offerSurrender;
     }
 
     public static class InvalidCoordinateException extends RuntimeException {
